f406911abc0b5eb97edf5d4d346515a79500fe9a
EthanCrisford <476052@chrome.escg.ac.uk>
2022-11-26 11:15:29 +0000

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,54 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    float xAxis;
-    float yAxis;
-    int groundMask;
-    bool isGrounded;
-    bool isJumping;
-    Animator animator;
-    private string currentState;
-    Rigidbody2D rb2d;
-    bool isAttacking;
-    bool isAttackPressed;
-
-    //Animaton States
-    const string idle = "idle";
-    const string run = "run";
-    const string jump = "jump";
-    const string attack = "attack";
-
+    private Rigidbody2D myRigidbody;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
-        groundMask = 1;
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void ChangeAnimationState(string newState)
-    {
-        //stop the same animation from interupting itself
-        if (currentState == newState) return;
-
-        //play the animation
-       animator.Play(newState);
-
-        //reassign the current state
-        currentState = newState;
-
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        xAxis = 
+        Movement();
+    }
 
-
-
-
+    private void Movement()
+    {
+        myRigidbody.velocity = Vector2.left; //x + -1, y = 0;
     }
 }
