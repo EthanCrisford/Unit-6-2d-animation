7894023fc1e20e3ac291c3efd5588ed21812bc19
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2022-11-28 10:07:21 +0000

    final base

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,35 +8,47 @@ public class Player : MonoBehaviour
     private Rigidbody2D myRigidbody;
     Animator animator;
     float horizontal;
-    private string currentState;
-    string idle;
-    string newState;
+    string currentState;
+    const string idle = "idle";
+    const string walk = "walk";
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        animator.Play("idle");
+        animator.Play(idle);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey("d") == true)
-
+        {
             myRigidbody.velocity = new Vector2(5, 0);
+            ChangeAnimationState("walk");
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(0, 0);
+            ChangeAnimationState("Idle");
+
+        }
 
     }
 
     void ChangeAnimationState(string newState)
     {
-        if (currentState == idle) return;
+        if (currentState == newState) return;
 
-        animator.Play("idle");
+        animator.Play(newState);
 
         currentState = newState;
 
     }
 
 }
+
+
+
+
