6ab813c6de723f9ca5db284148d49fac6066ac41
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2022-12-02 15:11:41 +0000

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,8 +10,10 @@ public class Player : MonoBehaviour
     Animator animator;
     float horizontal;
     string currentState;
-    const string idle = "idle";
+    const string idle = "Idle";
     const string walk = "walk";
+    public GameObject explosionPrefab;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         animator.Play(idle);
+        gameObject.tag = "Player";
     }
 
     // Update is called once per frame
@@ -33,7 +37,23 @@ public class Player : MonoBehaviour
             myRigidbody.velocity = new Vector2(0, 0);
             ChangeAnimationState("Idle");
         }
+
     }
+    
+    private void OnTriggerEnter2D(Collider2D diamond)
+    {
+        GameObject prefab;
+        if (diamond.tag == "Diamond")
+        {
+            // make a clone of the explosion prefab
+            prefab = Instantiate(explosionPrefab);
+
+            // set the position of the explosion to be the same as the diamond
+            prefab.transform.position = diamond.transform.position;
+            Destroy(diamond.gameObject);
+        }
+    }
+   
 
     void ChangeAnimationState(string newState)
     {
@@ -43,6 +63,8 @@ public class Player : MonoBehaviour
 
         currentState = newState;
     }
+
+   
 }
 
 
